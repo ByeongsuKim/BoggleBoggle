c12bcd5b652d5982122cb548d132876b5e2651bf
code.ssu <code.ssu@gmail.com>
2022-02-09 20:30:34 +0900

    CSS 적용

--- js/includeHTML.js
+++ js/includeHTML.js
@@ -35,3 +35,21 @@ function includeHTML(callback) {
     callback();
   }, 0);
 }
+
+//비동기식 웹폰트(나눔고딕) 다운로드
+  WebFontConfig = {
+    custom: {
+        families: ['Nanum Gothic'],
+        urls: ['http://fonts.googleapis.com/earlyaccess/nanumgothic.css']
+    }
+  };
+  
+  (function() {
+    var wf = document.createElement('script');
+    wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+      '://ajax.googleapis.com/ajax/libs/webfont/1.4.10/webfont.js';
+    wf.type = 'text/javascript';
+    wf.async = 'true';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(wf, s);
+  })(); 
